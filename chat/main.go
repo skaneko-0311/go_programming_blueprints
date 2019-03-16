8fa263b1bc505a5e6f5e7b8b1875bcea290d2ad2
skaneko-0311 <skaneko.0311@gmail.com>
2019-03-16 23:39:04 +0900

    1.4 Add logging method

--- chat/main.go
+++ chat/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
 	"text/template"
+
+	"../trace"
 )
 
 type templateHandler struct {
@@ -19,15 +23,19 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		t.templ =
 			template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse()
 	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	go r.run()
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Webサーバを開始します。ポート: ", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
